8882f936e1708d3106d91443f4c90ba7745f7801
[AnasAA98] <[aaffouteanas@gmail.com]>
2024-11-27 23:03:47 -0500

    app.py file modified

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pickle
+import joblib
+import numpy as np
 
+# Streamlit application title
 st.title("Air Quality Map")
 
 # Tableau Public URL
@@ -7,3 +11,28 @@ tableau_public_url = "https://public.tableau.com/views/Book1_17323934199370/Shee
 
 # Embed the Tableau Public dashboard
 st.components.v1.iframe(src=tableau_public_url, width=1100, height=850)
+
+# Load the model
+with open('src/saved_model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+# Add input fields to get user data
+st.subheader("AQI Prediction Input")
+state_name = st.text_input("Enter State Name")
+population = st.number_input("Enter Population", min_value=0, step=1)
+non_renewable_vehicles = st.number_input("Enter Number of Non-Renewable Vehicles", min_value=0, step=1)
+
+# Button to make predictions
+if st.button("Predict AQI"):
+    # Ensure that the user has entered a state name
+    if not state_name:
+        st.warning("Please enter a state name")
+    else:
+        # Prepare input data for prediction
+        input_features = np.array([population, non_renewable_vehicles]).reshape(1, -1)
+
+        # Make prediction using the model
+        prediction = loaded_model.predicted_aqi(input_features)
+
+        # Display the predicted AQI
+        st.success(f"The predicted Air Quality Index (AQI) for {state_name} is: {prediction[0]}")
